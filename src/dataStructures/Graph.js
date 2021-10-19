d7b95befae6c968c2b644a2fefefee1b603dee1b
Luis Martinez <luis_mmartinez@live.com>
2021-10-19 19:45:57 -0400

    add error handling to addVertex method in Graph class

--- src/dataStructures/Graph.js
+++ src/dataStructures/Graph.js
@@ -7,8 +7,13 @@ class Graph{
         if(!this.adjacencyList[vertext]) this.adjacencyList[vertex] = [];
     }
     addEdge(vertex1, vertex2){
-        this.adjacencyList[vertex1].push(vertex2);
-        this.adjacencyList[vertex2].push(vertex1);
+        if(
+            !this.adjacencyList[vertex1]?.includes(vertex2) && 
+            !this.adjacencyList[vertex2]?.includes(vertex1)
+        ){
+            this.adjacencyList[vertex1].push(vertex2);
+            this.adjacencyList[vertex2].push(vertex1);
+        }
     }
     removeEdge(vertex1, vertex2){
         this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
@@ -26,3 +31,20 @@ class Graph{
         delete this.adjacencyList[vertex];
     }
 }
+
+// let g = new Graph()
+// g.addVertex("A")
+// g.addVertex("B")
+// g.addVertex("C")
+// g.addVertex("D")
+// g.addVertex("E")
+// g.addVertex("F")
+
+// g.addEdge("A","B")
+// g.addEdge("A","C")
+// g.addEdge("B","D")
+// g.addEdge("C","E")
+// g.addEdge("D","E")
+// g.addEdge("D","F")
+// g.addEdge("E","F")
+// g
