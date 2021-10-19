0856e8f745936de12645fe868a7519e7c5383cdc
Luis Martinez <luis_mmartinez@live.com>
2021-10-18 23:20:59 -0400

    add Graph class to data structures

---
+++ src/dataStructures/Graph.js
@@ -0,0 +1,21 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = {}
+    }
+
+    addVertext(vertex){
+        if(!this.adjacencyList[vertext]) this.adjacencyList[vertex] = [];
+    }
+    addEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+    removeEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        )
+    }
+}
