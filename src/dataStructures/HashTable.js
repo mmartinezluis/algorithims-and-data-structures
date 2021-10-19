56c92c3d2b528d80654e680517910d0870ad21dc
Luis Martinez <luis_mmartinez@live.com>
2021-10-18 20:07:51 -0400

    add keys and values methods to HashTable class

--- src/dataStructures/HashTable.js
+++ src/dataStructures/HashTable.js
@@ -1,5 +1,5 @@
 class HashTable{
-    constructor(size=4){
+    constructor(size=11){
         this.keymap = new Array(size)
     }
 
@@ -21,16 +21,54 @@ class HashTable{
         } 
         this.keymap[index].push([key,value]);
     }
-    get(ley){
-        
+    get(key){
+        let index = this._hash(key);
+        if(this.keymap[index]){
+            for(let i=0; i< this.keymap[index].length; i++){
+                if(this.keymap[index][i][0] === key){
+                    return this.keymap[index][i][1];    
+                }
+            }
+        }
+        return undefined;
     }
-
+    keys(){
+        let data=[];
+        for(let i =0; i < this.keymap.length; i++){
+            if(this.keymap[i]){
+                let j=0;
+                while( j < this.keymap[i].length){
+                    if(!data.includes(this.keymap[i][j][0])){
+                        data.push(this.keymap[i][j][0])
+                        j++;
+                    }
+                }    
+            }
+            
+        }
+        return data;
+    }
+    values(){
+        let data = [];
+        for(let i=0; i< this.keymap.length; i++){
+            if(this.keymap[i]){
+                for(let j=0; j < this.keymap[i].length; j++){
+                    if(!data.includes(this.keymap[i][j][1])){
+                        data.push(this.keymap[i][j][1])
+                    }
+                }
+            }
+        }
+        return data;
+    }
 
     
 }
-
-// ht.set("trdfdfdfes","tresprimo")
-// ht.set("ddfdeeeos","dosprmo")
-// ht.set("ufdfdfno","uno prmo")
+// ht.values()
+// ht.keys()
+// ht.get("uno")
+// ht.set("uno","tresprimo")
+// ht.set("dos","dosprmo")
+// ht.set("tres","uno prmo")
 // ht
-// let ht = new HashTable();
+// let ht = new HashTable(7);
