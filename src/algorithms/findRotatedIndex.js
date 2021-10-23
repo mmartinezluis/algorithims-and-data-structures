77e69dca81bd33f8d74d24a7e2f69abdd8e2cd5c
Luis Martinez <luis_mmartinez@live.com>
2021-10-22 20:01:42 -0400

    solved findRotatedIndex algorithm problem

--- src/algorithms/findRotatedIndex.js
+++ src/algorithms/findRotatedIndex.js
@@ -1,29 +1,49 @@
-function findRotatedIndex(arr){
+// Lggic: First, find the index of rotation; thss will aloow us to split the array in two SORTED sub arrays;
+// note: the index of rotation is the maximum value in the array
+// then applly binary search on each sub array to see if the given value is found in any of the subarrays;
+// if value is found in first subarray, binary rearch function alone takes care of returning the corect index;
+// if values is found on second subarray, we need to start counting from the partition point (the rotatedIndex)
+// and add 1 to the returned value of binary search function.
+
+// Time complexity: log(n) [for finding the rotated index] + log(n) [for binary search] = log(n)
+
+function findRotatedIndex(arr, value){
     let i =0;
     let j = arr.length-1;
     let middle;
-    
+    if (arr[i] === value) return i;     //edge case
+    if (arr[j] === value) return j;     //edge case
     while( i!==j ){
-        middle = Math.floor((1+j)/2);
-        if(arr[j] < arr[i]){
-            j = middle;
-        } else {
+        middle = Math.floor((i+j)/2);
+        if(arr[middle] > arr[i]){
             i = middle;
-            if( j - i === 1){
-                arr[j] > arr[i] ? i=j : j=i;
-            }
-        }
+        } else j = middle;
     }
-    return j;
-  //   let rotatedIndex = i;
-  //   if( integer < arr[rotatedIndex] ){
-  //       return binarySearch(arr.slice(0, rotatedIndex));
-  //   } else if( integer > arr[rotatedIndex]){
-  //       let result = binarySearch(arr.slice(rotatedIndex+1));
-  //       return result === -1 ? -1 : rotatedIndex + result + 1;
-  //   } else return rotatedIndex;
+  
+    let rotatedIndex = i;
+    if (value > arr[rotatedIndex]) return -1;
+    if (value === arr[rotatedIndex]) return rotatedIndex;
+    if( value > arr[0]){
+        return binarySearch(arr.slice(0, rotatedIndex), value);
+    } else {
+        let result = binarySearch(arr.slice(rotatedIndex+1), value);
+        return result === -1 ? -1 : rotatedIndex + result + 1;
+    } 
   }
   
-  let arr = [13,14,22,33,44,99,100,333,4];
   
-  findRotatedIndex(arr)
+  function binarySearch(arr, value){
+    let start = 0;
+    let end = arr.length -1;
+    let middle = Math.floor((start + end)/2);
+    while( start <= end && arr[middle] !== value){
+      if( arr[middle] < value ) start = middle +1;
+      else end = middle -1;
+      middle = Math.floor( (end + start)/2 );
+    }
+    return arr[middle] === value ? middle : -1;
+  }
+
+
+// let arr = [13,14,22,33,44,99,333,444,2,3,4];
+// findRotatedIndex(arr,424)
