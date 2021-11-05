4a0b41ed5388ba0ee8fd0df5683314f3bda42003
Luis Martinez <luis_mmartinez@live.com>
2021-11-05 17:46:13 -0400

    add isSubPath problem; unresolved

---
+++ src/dataStructures/unsolved/linkedListinBinaryTree.js
@@ -0,0 +1,27 @@
+// Given a binary tree root and a linked list with head as the first node. 
+
+// Return True if all the elements in the linked list starting from the head correspond to some downward path connected in the binary tree otherwise return False.
+
+// In this context downward path means a path that starts at some node and goes downwards.
+
+
+// This code passes 63/67 cases in leetCode; needs improvement;
+
+var isSubPath = function(head, root) { 
+    let list = head
+    let node = root;
+    let map;
+    function traverseTree(node, list){       
+        if(node.val === list.val ){
+            list = list.next;       
+        } 
+        else {
+            list = head;
+        }        
+        if(!list) return map= true;
+        if(node.left) traverseTree(node.left, list) 
+        if(node.right) traverseTree(node.right, list);   
+    }
+     traverseTree(node, list);
+     return !!map;
+};
