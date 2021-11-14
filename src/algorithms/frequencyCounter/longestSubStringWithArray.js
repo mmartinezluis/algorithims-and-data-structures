942eeb45a70754af8196c529977eedda11ea4921
Luis Martinez <luis_mmartinez@live.com>
2021-11-14 10:03:43 -0500

    add longestSubStringWithArray to algorithms

---
+++ src/algorithms/frequencyCounter/longestSubStringWithArray.js
@@ -0,0 +1,18 @@
+function lengthOfLongestSubstring(str) {
+    let currentString = [];
+    let longestSubstring = 0;
+
+    for(let i=0; i < str.length; i++) {
+
+        const currentCharacterPosition = currentString.indexOf(str[i])
+        if(currentCharacterPosition !== -1) {
+            currentString = [];
+        }
+        currentString.push(str[i]);
+        longestSubstring = Math.max(longestSubstring, currentString.length)
+    }
+    return longestSubstring;
+}
+
+let string = "g"
+lengthOfLongestSubstring(string)
