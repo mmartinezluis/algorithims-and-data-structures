c6ff9bd74b8af04904ea41382e3207291c5e5fa5
Luis Martinez <luis_mmartinez@live.com>
2021-11-16 20:05:52 -0500

    add unsolved problem 'theMaze' to graphMatrix

---
+++ src/dataStructures/graphMatrix/theMaze.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} maze
+ * @param {number[]} start
+ * @param {number[]} destination
+ * @return {boolean}
+ */
+ var hasPath = function(maze, start, destination) {
+    let parthMap = new Map();
+    
+    function move(start){
+        
+    }
+};
