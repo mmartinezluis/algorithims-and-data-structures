8cd267665bbbf3bd64b46a6f589cb741ad0127e6
Luis Martinez <luis_mmartinez@live.com>
2022-07-15 23:57:33 -0400

    trying solving diameter of binary tree

--- src/dataStructures/binaryTrees/diameterBinaryTree.js
+++ src/dataStructures/binaryTrees/diameterBinaryTree.js
@@ -10,6 +10,28 @@
  * @param {TreeNode} root
  * @return {number}
  */
- var diameterOfBinaryTree = function(root) {
-    
+var diameterOfBinaryTree = function(root) {
+    if(!root) return 0;
+    let counter = 0;
+    let branchCounter = 0;
+    (function repeat(node, tempCount) {
+        counter = Math.max(counter, tempCount)
+        if(node.left !== null && node.right !== null) {
+            branchCounter = Math.max(nodeMaxDepth(node.left) + nodeMaxDepth(node.right), branchCounter);
+        }
+        if(node.left)  repeat(node.left, tempCount + 1);
+        if(node.right)  repeat(node.right, tempCount + 1);
+    })(root, 0)
+    return Math.max(counter, branchCounter);
 };
+
+
+function nodeMaxDepth(node) {
+    let counter = 0;
+     (function recursive(node, tempCount){
+        counter = Math.max(counter, tempCount);
+        if(node.left) recursive(node.left, tempCount + 1);
+        if(node.right) recursive(node.right, tempCount + 1);
+     })(node, counter)
+     return counter;
+}
