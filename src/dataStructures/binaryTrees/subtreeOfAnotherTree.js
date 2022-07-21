38fd64ad1049285d0f44ac5dfb141c8d2c35f561
Luis Martinez <luis_mmartinez@live.com>
2022-07-20 23:55:38 -0400

    solving subtree of another tree

---
+++ src/dataStructures/binaryTrees/subtreeOfAnotherTree.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+ var isSubtree = function(root, subRoot) {
+    if(isSameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+};
