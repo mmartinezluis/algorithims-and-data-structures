62b7c40cd11dbc7e27bad7c1efb74c0b8478a7db
Luis Martinez <luis_mmartinez@live.com>
2022-07-21 08:58:04 -0400

    solved subtree of another tree; need to derive an optimal solution still

--- src/dataStructures/binaryTrees/subtreeOfAnotherTree.js
+++ src/dataStructures/binaryTrees/subtreeOfAnotherTree.js
@@ -11,7 +11,20 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
+// This solution works, but is not optimal
  var isSubtree = function(root, subRoot) {
+    if(!root && !subRoot) return true;
+    if((!root && subRoot) || (root & !subRoot)) return false;
     if(isSameTree(root, subRoot)) return true;
     return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
 };
+
+function isSameTree(p, q) {
+    if(p === null && q === null) {
+        return true;
+    } else if(p !== null && q !== null) {
+        if(p.val === q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else return false;
+    } else return false;
+}
