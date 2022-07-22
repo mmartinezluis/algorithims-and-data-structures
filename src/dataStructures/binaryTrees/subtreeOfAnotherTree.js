b8930608021b3f48822cd890efe7e7c8e0c30bf0
Luis Martinez <luis_mmartinez@live.com>
2022-07-22 11:51:00 -0400

    solved lowest common acnestor of binary search tree, recursive

--- src/dataStructures/binaryTrees/subtreeOfAnotherTree.js
+++ src/dataStructures/binaryTrees/subtreeOfAnotherTree.js
@@ -11,7 +11,7 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
-// This solution works, but is not optimal
+// This solution works, but it is not optimal
  var isSubtree = function(root, subRoot) {
     if(!root && !subRoot) return true;
     if((!root && subRoot) || (root & !subRoot)) return false;
@@ -27,4 +27,17 @@ function isSameTree(p, q) {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         } else return false;
     } else return false;
+}
+
+// A bit improved
+var isSubtree = function(root, subRoot) {
+    if(!root) return false;
+    if(isSameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+};
+
+function isSameTree(p, q) {
+    if(!p && !q )return true;
+    if(!p || !q || p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
