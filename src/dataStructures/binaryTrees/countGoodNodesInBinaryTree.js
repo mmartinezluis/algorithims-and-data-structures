db742ef1e115ebac7debb405f0d8b2e97b1adf5f
Luis Martinez <luis_mmartinez@live.com>
2022-07-22 23:07:28 -0400

    solved count goood nodes in binary tree

--- src/dataStructures/binaryTrees/countGoodNodesInBinaryTree.js
+++ src/dataStructures/binaryTrees/countGoodNodesInBinaryTree.js
@@ -11,5 +11,24 @@
  * @return {number}
  */
  var goodNodes = function(root) {
-    
+    let max = root.val;
+    let count = 1;
+
+    const traverse = function(node, max) {
+        if(node.left) {
+            if(node.left.val >= max) {
+                count++;
+                traverse(node.left, node.left.val);
+            } else traverse(node.left, max);
+        }
+        
+        if(node.right) {
+            if(node.right.val >= max) {
+                count++;
+                traverse(node.right, node.right.val);
+            } else traverse(node.right, max);
+        }
+    }
+    traverse(root, max);
+    return count;
 };
