36b88a637fcedd698a3102891fe1f65dfd88f7cb
Luis Martinez <luis_mmartinez@live.com>
2022-07-23 16:19:09 -0400

    solved is valid BST

--- src/dataStructures/binaryTrees/validBinarySearchTree.js
+++ src/dataStructures/binaryTrees/validBinarySearchTree.js
@@ -10,6 +10,34 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+// first attempt-Does not work
  var isValidBST = function(root) {
-    
+    let max = min = root.val;
+    return validLeftBranch(root.left, Number.MAX_SAFE_INTEGER) && validRightBranch(root.right);
+
+    function validLeftBranch(node, max) {
+        if(!node) return true;
+        if((node.left && node.left.val > node.val) || (node.right && node.right.val < node.val) || node.val >= max ) return false;
+        return validLeftBranch(node.left) && validLeftBranch(node.right);
+    }
+    function validRightBranch(node) {
+        if(!node) return true;
+        if((node.left && node.left.val > node.val) || (node.right && node.right.val < node.val) || node.val <= min ) return false;
+        return validRightBranch(node.left) && validRightBranch(node.right);
+    }
 };
+
+// Working solution; this problem is very tricky to initialize
+var isValidBST = function(root) {
+    return validate(root, null, null);
+
+    function validate(node, low, high) {
+        if(!node) return true;
+        if(low !== null && node.val <= low) return false;
+        if(high !== null && node.val >= high) return false;
+        return validate(node.left, low, node.val) && validate(node.right, node.val, high);
+    }
+}
+
+
+
