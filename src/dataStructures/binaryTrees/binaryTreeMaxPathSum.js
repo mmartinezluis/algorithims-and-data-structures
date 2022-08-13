83446ada4d46c07dad466f9e0657d704afd09781
Luis Martinez <luis_mmartinez@live.com>
2022-08-12 23:22:07 -0400

    solved max path sum binary tree

--- src/dataStructures/binaryTrees/binaryTreeMaxPathSum.js
+++ src/dataStructures/binaryTrees/binaryTreeMaxPathSum.js
@@ -10,16 +10,23 @@
  * @param {TreeNode} root
  * @return {number}
  */
- var maxPathSum = function(root) {
+var maxPathSum = function(root) {
     let sum = Number.MIN_SAFE_INTEGER;
+    let previousSum;
+    let path = [];
+
     function heightSum(node) {
         if(!node) return 0;
-        const leftSum = heightSum(node.left);
-        const rightSum = heightSum(node.right);
-
-        sum = Math.max(sum, leftSum + rightSum);
+        const leftSum = Math.max(heightSum(node.left), 0);
+        const rightSum = Math.max(heightSum(node.right), 0);
+        
+        previousSum = sum;
+        sum = Math.max(sum, leftSum + rightSum + node.val);
+        if(previousSum !== sum) path.push(node.val)
         return node.val + Math.max(leftSum, rightSum);
     }
+    
     heightSum(root);
     return sum;
 };
+
