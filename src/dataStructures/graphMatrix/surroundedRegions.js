91aea38b99abf3bf5b6d1fe4b909e2fa7dc23a63
Luis Martinez <luis_mmartinez@live.com>
2022-08-18 23:27:57 -0400

    refactored surrounded regions problem to optimal solution

--- src/dataStructures/graphMatrix/surroundedRegions.js
+++ src/dataStructures/graphMatrix/surroundedRegions.js
@@ -3,22 +3,24 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
  var solve = function(board) {
-    const flip = new Array(board.length).fill(null).map( row => new Array(board[0].length).fill(true))
     const nr = board.length;
     const nc = board[0].length;
 
-    for(let i =0; i< nr; i++) {
-        dfs(i, 0, board, flip)
-        dfs(i, nc -1, board, flip)
+    for(let i = 0; i< nr; i++) {
+        dfs(i, 0, board)
+        dfs(i, nc -1, board)
     }
-    for(let j=0; j < nc; j++) {
-        dfs(0, j, board, flip)
-        dfs(nr -1, j, board, flip)
+    for(let j = 0; j < nc; j++) {
+        dfs(0, j, board)
+        dfs(nr -1, j, board)
     }
 
+    let value;
     for(let k=0; k < nr; k++) {
         for(let w=0; w < nc; w++) {
-            regions(k, w, board, flip)
+            value = board[k][w];
+            if(value == "O") board[k][w] = "X"
+            if(value == "E") board[k][w] ="O"
         }
     }
 };
@@ -27,28 +29,16 @@ const directions = [
     [0,1], [0,-1], [-1,0], [1,0]
 ]
 
-function dfs(i, j, board, flip) {
-    if(board[i][j] == "O" && flip[i][j]) {
-        flip[i][j] = false;
+function dfs(i, j, board) {
+    if(board[i][j] == "O") {
+        board[i][j] = "E";
         for(let k=0; k < directions.length; k++) {
             let dir = directions[k];
             let x = i + dir[0];
             let y = j + dir[1];
-            if(x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] == "X" || !flip[x][y]) continue;
-            dfs(x, y, board, flip)
+            if(x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] == "X" || board[x][y] == "E") continue;
+            dfs(x, y, board)
         }
     }
 }
 
-function regions(i, j, board, flip) {
-    if(board[i][j] == "O" && flip[i][j]) {
-        board[i][j] = "X";
-        for(let k=0; k < directions.length; k++) {
-            let dir = directions[k];
-            let x = i + dir[0];
-            let y = j + dir[1];
-            if(x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] == "X" || !flip[x][y]) continue;
-            regions(x, y, board, flip)
-        }
-    }
-}
