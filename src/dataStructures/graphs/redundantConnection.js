c70eedefdb8d83319fb9a74b0ed51da05f8c2c11
Luis Martinez <luis_mmartinez@live.com>
2022-08-30 23:58:46 -0400

    refactor adjacency list to use a map instead of an object

--- src/dataStructures/graphs/redundantConnection.js
+++ src/dataStructures/graphs/redundantConnection.js
@@ -5,18 +5,28 @@
 
  var findRedundantConnection = function(edges) {
     
-    this.adjList = {};
-
-    function addVertex(vertex) {
-        if(!this.adjList[vertex]) this.adjList[vertex] = [];
-    }
-
+    this.adjList = new Map();
+    
     function addEdge(edge) {
         const v1 = edge[0];
         const v2 = edge[1];
-        if(!this.adjList[v1]) this.adjList[v1] = [];
-        if(!this.adjList[v2]) this.adjList[v2] = [];
-        this.adjList[v1].push(v2);
-        this.adjList[v2].push(v1);
+        if(!this.adjList.has(v1)) this.adjList.set(v1, []);
+        if(!this.adjList.has(v2)) this.adjList.set(v2, []);
+        this.adjList.get(v1).push(v2);
+        this.adjList.get(v2).push(v1);
     }
+
+    for(let edge of edges) {
+        addEdge(edge);
+    }
+
+    for(let vertex in this.adjList) {
+        const neighbors = this.adjList[vertex];
+        if(neighbors.length > 1) {
+            for(let neighbor of neighbors) {
+                if(this.adjList[neighbor].length > 1) return [vertex, neighbor];
+            }
+        }
+    }
+
 };
