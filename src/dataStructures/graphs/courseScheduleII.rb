a5571ce12df4d121f9927921dc27b2163bb4d16c
Luis Martinez <luis_mmartinez@live.com>
2022-09-04 16:20:09 -0400

    solved course schedule II using Khan's algorithm, Ruby

--- src/dataStructures/graphs/courseScheduleII.rb
+++ src/dataStructures/graphs/courseScheduleII.rb
@@ -62,7 +62,7 @@ num_courses = 4
 prerequisites = [
     [1,0],[2,0],[3,1],[3,2]
 ]
-find_order(num_courses, prerequisites)
+# find_order(num_courses, prerequisites)
 
 
 
@@ -150,61 +150,89 @@ find_order(num_courses, prerequisites)
 # }
 
 
+# Ruby, using Khan's algorithm
+def find_order(num_courses, prerequisites)
+    adj_list = {}
+    indegree = Array.new(num_courses) {0}
+    topological_order = []
+    queue = []
+
+    prerequisites.each do |pre|
+        src = pre[1]
+        dest = pre[0]
+        adj_list[src] ? adj_list[src] << dest : adj_list[src] = [dest]
+        indegree[dest] +=1
+    end
+
+    num_courses.times {|i| queue << i if indegree[i] == 0}
+    
+    while !queue.empty?
+        node = queue.shift
+        (adj_list[node] || []).each do |neighbor|
+            indegree[neighbor] -=1
+            queue << neighbor if indegree[neighbor] == 0
+        end
+        topological_order << node
+    end
+    num_courses == topological_order.length ? topological_order : []
+end  
+
+find_order(num_courses, prerequisites)
 
 
 # Java, Khan's algorithm
-class Solution {
-  public int[] findOrder(int numCourses, int[][] prerequisites) {
+# class Solution {
+#   public int[] findOrder(int numCourses, int[][] prerequisites) {
 
-    boolean isPossible = true;
-    Map<Integer, List<Integer>> adjList = new HashMap<Integer, List<Integer>>();
-    int[] indegree = new int[numCourses];
-    int[] topologicalOrder = new int[numCourses];
+#     boolean isPossible = true;
+#     Map<Integer, List<Integer>> adjList = new HashMap<Integer, List<Integer>>();
+#     int[] indegree = new int[numCourses];
+#     int[] topologicalOrder = new int[numCourses];
 
-    // Create the adjacency list representation of the graph
-    for (int i = 0; i < prerequisites.length; i++) {
-      int dest = prerequisites[i][0];
-      int src = prerequisites[i][1];
-      List<Integer> lst = adjList.getOrDefault(src, new ArrayList<Integer>());
-      lst.add(dest);
-      adjList.put(src, lst);
+#     # Create the adjacency list representation of the graph
+#     for (int i = 0; i < prerequisites.length; i++) {
+#       int dest = prerequisites[i][0];
+#       int src = prerequisites[i][1];
+#       List<Integer> lst = adjList.getOrDefault(src, new ArrayList<Integer>());
+#       lst.add(dest);
+#       adjList.put(src, lst);
 
-      // Record in-degree of each vertex
-      indegree[dest] += 1;
-    }
+#     #   Record in-degree of each vertex
+#       indegree[dest] += 1;
+#     }
 
-    // Add all vertices with 0 in-degree to the queue
-    Queue<Integer> q = new LinkedList<Integer>();
-    for (int i = 0; i < numCourses; i++) {
-      if (indegree[i] == 0) {
-        q.add(i);
-      }
-    }
+#     # Add all vertices with 0 in-degree to the queue
+#     Queue<Integer> q = new LinkedList<Integer>();
+#     for (int i = 0; i < numCourses; i++) {
+#       if (indegree[i] == 0) {
+#         q.add(i);
+#       }
+#     }
 
-    int i = 0;
-    // Process until the Q becomes empty
-    while (!q.isEmpty()) {
-      int node = q.remove();
-      topologicalOrder[i++] = node;
+#     int i = 0;
+#     # Process until the Q becomes empty
+#     while (!q.isEmpty()) {
+#       int node = q.remove();
+#       topologicalOrder[i++] = node;
 
-      // Reduce the in-degree of each neighbor by 1
-      if (adjList.containsKey(node)) {
-        for (Integer neighbor : adjList.get(node)) {
-          indegree[neighbor]--;
+#     #   Reduce the in-degree of each neighbor by 1
+#       if (adjList.containsKey(node)) {
+#         for (Integer neighbor : adjList.get(node)) {
+#           indegree[neighbor]--;
 
-          // If in-degree of a neighbor becomes 0, add it to the Q
-          if (indegree[neighbor] == 0) {
-            q.add(neighbor);
-          }
-        }
-      }
-    }
+#         #    If in-degree of a neighbor becomes 0, add it to the Q
+#           if (indegree[neighbor] == 0) {
+#             q.add(neighbor);
+#           }
+#         }
+#       }
+#     }
 
-    // Check to see if topological sort is possible or not.
-    if (i == numCourses) {
-      return topologicalOrder;
-    }
+#     # Check to see if topological sort is possible or not.
+#     if (i == numCourses) {
+#       return topologicalOrder;
+#     }
 
-    return new int[0];
-  }
-}
+#     return new int[0];
+#   }
+# }
