fe8a03be81887426b27fcc58e5b9c9bd8dbfd63d
Luis Martinez <luis_mmartinez@live.com>
2022-09-09 08:59:45 -0400

    solving reorder list, JavaScript

--- src/dataStructures/singlyLinkedList/reorderList.js
+++ src/dataStructures/singlyLinkedList/reorderList.js
@@ -11,4 +11,25 @@
  */
  var reorderList = function(head) {
     
+    if(!head) return null;
+
+    head.next = helper(head);
+
+    function helper(node) {
+        const reference = node;
+        while(reference.next) {
+            reorderList(reference.next)
+            reference = reference.next
+        }
+        return reference;
+    }
+    return head
 };
+
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+ListNode(4)
