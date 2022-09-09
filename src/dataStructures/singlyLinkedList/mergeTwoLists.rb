1f6f59052b60a48e48006d65fccb724017f4fd10
Luis Martinez <luis_mmartinez@live.com>
2022-09-08 23:36:36 -0400

    solved merge two linked lists, Ruby

--- src/dataStructures/singlyLinkedList/mergeTwoLists.rb
+++ src/dataStructures/singlyLinkedList/mergeTwoLists.rb
@@ -15,20 +15,25 @@ def merge_two_lists(list1, list2)
     return nil if !node1 && !node2
     return node1 if node1 && !node2
     return node2 if !node1 && node2
-    root = node2.val >= node1.val ? ListNode.new(node1.val) : ListNode.new(node2.val)
-
+    if node2.val >= node1.val  
+        root = ListNode.new(node1.val) 
+        node1 = node1.next
+    else 
+        root = ListNode.new(node2.val)
+        node2 = node2.next
+    end
+    final = root
     while node1 && node2
         if node1.val <= node2.val
-            if node1.next && node1.next.val <= node2.val
-                root.next = node1.next
-                node1 = node1.next
-            end
+            root.next = node1
+            root = node1
+            node1 = node1.next
         else
             root.next = node2
+            root = node2
             node2 = node2.next
         end
     end
-        
-    root.next = !node1 ? node2.next : node1.next
-    root
+    root.next = !node1 ? node2 : node1
+    final
 end
