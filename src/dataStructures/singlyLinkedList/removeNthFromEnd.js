28db98623f032b4d44c129b65d00da0fef290ed6
Luis Martinez <luis_mmartinez@live.com>
2022-09-13 08:49:38 -0400

    solved removeNthFromEnd, non optimal, JavaScript

--- src/dataStructures/singlyLinkedList/removeNthFromEnd.js
+++ src/dataStructures/singlyLinkedList/removeNthFromEnd.js
@@ -10,40 +10,57 @@
  * @param {number} n
  * @return {ListNode}
  */
+
+// Solution works, but is is non optimal
  var removeNthFromEnd = function(head, n) {
     let reversed = head;
-    let prev, temp;
+    let prev = null, temp;
     while(reversed) {
         temp = reversed.next;
         reversed.next = prev;
         prev = reversed;
-        reversed = temp
+        reversed = temp;
     }
     reversed = prev;
-
+    
     let i = 1;
     let prev2;
+    let reference = reversed;
     while(i !== n) {
         i++;
-        prev2 = reversed;
-        reversed = reversed.next;
+        prev2 = reference;
+        reference = reference.next;
     }
 
     if(!prev2) {
         reversed = reversed.next;
     }
     else {
-        prev2.next = reversed.next;
+        prev2.next = reference.next;
     }
 
     let current = reversed;
     let prev3 = null;
     while(current) {
         temp = current.next;
-        current.next = prev;
+        current.next = prev3;
         prev3 = current;
         current = temp;
     }
 
     return prev3;
 };
+
+console.log(JSON.stringify(prev3, null, 2), JSON.stringify(reversed, null, 2));
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+let head = new ListNode(1);
+const two = head.next = new ListNode(2);
+const three = two.next = new ListNode(3);
+const four = three.next = new ListNode(4);
+four.next = new ListNode(5);
+
+removeNthFromEnd(head, 5)
