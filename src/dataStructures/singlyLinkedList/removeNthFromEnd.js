570d5200a9ee41c51d978c31ecc5a0035483f4b1
Luis Martinez <luis_mmartinez@live.com>
2022-09-13 00:58:19 -0400

    solving remove nth from end

--- src/dataStructures/singlyLinkedList/removeNthFromEnd.js
+++ src/dataStructures/singlyLinkedList/removeNthFromEnd.js
@@ -11,5 +11,39 @@
  * @return {ListNode}
  */
  var removeNthFromEnd = function(head, n) {
-    
+    let reversed = head;
+    let prev, temp;
+    while(reversed) {
+        temp = reversed.next;
+        reversed.next = prev;
+        prev = reversed;
+        reversed = temp
+    }
+    reversed = prev;
+
+    let i = 1;
+    let prev2;
+    while(i !== n) {
+        i++;
+        prev2 = reversed;
+        reversed = reversed.next;
+    }
+
+    if(!prev2) {
+        reversed = reversed.next;
+    }
+    else {
+        prev2.next = reversed.next;
+    }
+
+    let current = reversed;
+    let prev3 = null;
+    while(current) {
+        temp = current.next;
+        current.next = prev;
+        prev3 = current;
+        current = temp;
+    }
+
+    return prev3;
 };
