82a6f8cdbd5de27897ce215e0c82c46c73434813
Luis Martinez <luis_mmartinez@live.com>
2022-09-13 23:06:58 -0400

    add one pass solution for remove nth from list; JavaScript

--- src/dataStructures/singlyLinkedList/removeNthFromEnd.js
+++ src/dataStructures/singlyLinkedList/removeNthFromEnd.js
@@ -51,11 +51,87 @@
     return prev3;
 };
 
-console.log(JSON.stringify(prev3, null, 2), JSON.stringify(reversed, null, 2));
-function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+
+
+var removeNthFromEnd2 = function(head, n) {
+
+    // Make one pass to get list length
+    let length = 0;
+    let temp = head;
+    while(temp) {
+        temp = temp.next;
+        length += 1;
+    }
+
+    // Loop until getting to target node
+    const target = length - n + 1;
+    let prev = null;
+    let current = head;
+    let counter = 1;
+    while(counter !== target) {
+        prev = current;
+        current = current.next;
+        counter++;
+    }
+
+    // If target node was last node (from right to left), 
+    // move the head one step forward
+    !prev ? head = head.next : prev.next = current.next;
+
+    console.log(JSON.stringify(head, null, 2))
+
+    return head;
+
 }
+
+
+var removeNthFromEnd3 = function(head, n) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let length = 0;
+    let first = head;
+    while(first) {
+        length++;
+        first = first.next;
+    }
+
+    length -= n;
+    first = dummy;
+    while(length > 0) {
+        length--;
+        first = first.next;
+    }
+
+    first.next = first.next.next;
+    return dummy.next;
+
+}
+
+var removeNthFromEnd4 = function(head, n) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let first = dummy;
+    let second = dummy;
+
+    for(let i=1; i <= n+1; i++) {
+        first = first.next;
+    }
+    
+    while(first) {
+        first = first.next;
+        second = second.next;
+    }
+    
+    second.next = second.next.next;
+    return dummy.next;
+}
+
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
 
 let head = new ListNode(1);
 const two = head.next = new ListNode(2);
@@ -63,4 +139,4 @@ const three = two.next = new ListNode(3);
 const four = three.next = new ListNode(4);
 four.next = new ListNode(5);
 
-removeNthFromEnd(head, 5)
+removeNthFromEnd4(head, 5)
