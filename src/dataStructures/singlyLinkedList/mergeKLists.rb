86adc5a315bf8a0f88fc7256cb4647da0c98da16
Luis Martinez <luis_mmartinez@live.com>
2022-09-27 19:42:33 -0400

    solved merge K lists, iterative, modify lists in place, Ruby

--- src/dataStructures/singlyLinkedList/mergeKLists.rb
+++ src/dataStructures/singlyLinkedList/mergeKLists.rb
@@ -1,13 +1,15 @@
+require 'byebug'
 # Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val = 0, _next = nil)
-#         @val = val
-#         @next = _next
-#     end
-# end
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
 # @param {ListNode[]} lists
 # @return {ListNode}
+# Recursive; O(log k) space complexity
 def merge_k_lists(lists)
     return nil if lists.length == 0
     return lists[0] if lists.length == 1
@@ -18,6 +20,26 @@ def merge_k_lists(lists)
         i +=2
     end
     return merge_k_lists(queue)
+end
+
+# Iterative solution; modifying the given list in place
+def merge_k_lists2(lists)
+    return nil if lists.length == 0
+    k = lists.length
+    w = k
+    while w > 1
+        w = (w.to_f/2).ceil
+        i = 0
+        w.times { |index|
+            l1 = lists[i]
+            l2 = i + 1 < k ? lists[i+1] : nil
+            list = merge(l1, l2)
+            lists[index] = list
+            i +=2 
+        }
+        k = w
+    end
+    return lists[0]
 end
 
 def merge(l1, l2)
@@ -31,3 +53,18 @@ def merge(l1, l2)
         return l2
     end
 end
+
+l1 = ListNode.new(1)
+l1.next= ListNode.new(4)
+l1.next.next  = ListNode.new(5)
+
+l2 = ListNode.new(1)
+l2.next = ListNode.new(3)
+l2.next.next = ListNode.new(4)
+
+l3 = ListNode.new(2)
+l3.next = ListNode.new(6)
+
+lists = [l1,l2,l3]
+
+merge_k_lists2(lists)
