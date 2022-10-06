779af81a8e97d476b9353f74f917cb59850bfdf9
Luis Martinez <luis_mmartinez@live.com>
2022-10-06 00:54:16 -0400

    sovled binary search

--- src/algorithms/binarySearch/binarySearch.js
+++ src/algorithms/binarySearch/binarySearch.js
@@ -4,5 +4,17 @@
  * @return {number}
  */
  var search = function(nums, target) {
-    
+     if(!nums.length) return -1;
+     if(nums.length === 1 && nums[0] === target) return 0;
+    let i=0;
+    let j= nums.length-1;
+    let pointer;
+    while(i <= j) {
+        pointer = Math.floor((i+j)/2);
+        if(nums[pointer] === target) return pointer;
+        if(nums[pointer] < target) {
+            i = pointer+1;
+        } else j = pointer-1;
+    }
+    return -1;
 };
