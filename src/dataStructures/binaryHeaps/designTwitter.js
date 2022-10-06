2b75db5c6d487606fcf07957cf51b4b2baa6c367
Luis Martinez <luis_mmartinez@live.com>
2022-10-05 23:56:45 -0400

    solving design Twitter; javascript

--- src/dataStructures/binaryHeaps/designTwitter.js
+++ src/dataStructures/binaryHeaps/designTwitter.js
@@ -9,37 +9,43 @@ var Twitter = function() {
  * @return {void}
  */
 Twitter.prototype.postTweet = function(userId, tweetId) {
-    let timeStamp = +new Date();
     let tweet = {};
     tweet.id = tweetId;
-    tweet.timeStamp = timeStamp;
+    tweet.timeStamp = +new Date();
+    tweet.user = userId;
     if(this.users.has(userId)){
         this.users.get(userId).tweets.push(tweet);
         this.addToFeed(userId, tweet);
-        for(followerId of this.get(userId).followers) {
-            this.addToFeed(followerId, tweet);
-        }
     } else {
-        let data = {};
-        data.tweets = [tweet];
-        data.followees = [];
-        data.followers = [];
-        data.newsFeed = [];
-        this.users.set(userId, data);
+        // create user if not existing
+        this.createUser(userId, tweet)
+    }
+    // Rare, but user may already be followed without being created
+    for(followerId of this.get(userId).followers) {
+        this.addToFeed(followerId, tweet);
     }
 };
+
+Twitter.prototype.createUser = function (userId, tweet=null) {
+    let data = {}
+    data.tweets = tweet ? [tweet] : [];
+    data.followees = [];
+    data.followers = [];
+    data.newsFeed = tweet ? new MinBinaryHeap([tweet]) : new MinBinaryHeap();
+    this.users.set(userId, data);
+}
 
 /** 
  * @param {number} userId
  * @return {number[]}
  */
 Twitter.prototype.getNewsFeed = function(userId) {
-    const tweets = this.users.get(userId).newsFeed;
-    let result = [];
-    for(let i = tweets.length-1; i >= 0; i--) {
-        result.push(tweets[i]);
-    }
-    return result;
+    return this.users.get(userId).newsFeed.values
+        .slice()
+        .sort((a,b) => b.timeStamp - a.timeStamp)
+        .map(({id}) => {
+            return id;
+        });
 };
 
 /** 
@@ -49,6 +55,23 @@ Twitter.prototype.getNewsFeed = function(userId) {
  */
 Twitter.prototype.follow = function(followerId, followeeId) {
     this.users.get(followerId).followees.push(followeeId);
+    // Handle the follwee tweets
+    let userFeed = this.users.get(followerId).newsFeed;
+    let followeeUser
+    if(this.users.has(followeeId)) {
+
+    }
+
+    if(followeeTweets && followeeTweets.tweets.length > 0) {
+        followeeTweets = followeeTweets.tweets;
+        const user_min_tweet = userFeed.values[0];
+        const last_tweet = followeeTweets[followeeTweets.length -1];
+        if(userFeed.length < 10 || (last_tweet.timeStamp > user_min_tweet.timeStamp)) {
+            for(let i=followeeTweets.length-1; (i >= followeeTweets.length - 1 - 10) && i >= 0; i--) {
+                this.addToFeed(followerId, followeeTweets[i])
+            }
+        }
+    }
 };
 
 /** 
@@ -57,15 +80,24 @@ Twitter.prototype.follow = function(followerId, followeeId) {
  * @return {void}
  */
 Twitter.prototype.unfollow = function(followerId, followeeId) {
-    this.users.get(followerId).followees.push(followeeId);  
+    const updatedFollowees = this.users.get(followerId).followees.filter(id => id !== followeeId);
+    const feed = this.users.get(followerId).newsFeed;
+    feed.values = feed.values.filter(({user}) => user !== followeeId);
+    this.users.set(followerId, 
+        {...this.users.get(followerId), 
+            followees: updatedFollowees, 
+            newsFeed: feed
+        }
+    )
 };
 
 Twitter.prototype.addToFeed = function(userId, tweet) {
-    const length = this.users.get(userId).newsFeed.length;
-    this.users.get(userId).newsFeed.push(tweet);
-    if(length > 9) {
-        this.users.get(userId).tweets.shift();
+    const feed = this.users.get(userId).newsFeed;
+    feed.add(tweet);
+    if(feed.values.length > 10) {
+        feed.extractMin();
     }
+    this.users.set(userId, {...this.users.get(userId), newsFeed: feed});
 }
 
 /** 
@@ -77,4 +109,53 @@ Twitter.prototype.addToFeed = function(userId, tweet) {
  * obj.unfollow(followerId,followeeId)
  */
 
+class MinBinaryHeap {
+    constructor(values){
+        this.values = values || [];
+    }
+
+    add(val) {
+        this.values.push(val);
+        this.bubbleUp();
+    }
+    bubbleUp() {
+        let index = this.values.length-1;
+        while(index > 0) {
+            const node = this.values[index].timeStamp;
+            let parentIndex = Math.floor((index-1)/2);
+            let parent = this.values[parentIndex].timeStamp;
+            if(parent < node) break;
+            swap(this.values, parentIndex, index);
+            index = parentIndex;
+        }
+    }
+    extractMin() {
+        const min = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0) {
+            this.values[0] = end;
+            this.bubbleDown();
+        }
+        return min;
+    }
+    bubbleDown() {
+        let index = 0;
+        while(index < this.values.length -1) {
+            let swapIndex = null;
+            const node = this.values[index].timeStamp;
+            let childIndex = 2*index + 1;
+            let child1 = this.values[childIndex];
+            let child2 = this.values[childIndex +1];
+            if(child1 && node > child1.timeStamp) swapIndex = childIndex;
+            if(child2 && node > child2.timeStamp && child2.timeStamp < child1.timeStamp) swapIndex = childIndex +1;
+            if(swapIndex === null) break;
+            swap(this.values, swapIndex, index);
+            index = swapIndex;
+        }
+    }
+}
+
+function swap(arr, index1, index2) {
+    [ arr[index1], arr[index2] ] = [ arr[index2], arr[index1] ];
+}
 
