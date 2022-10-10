c9bc3f240febda68d3d0621713110f6669b6da54
Luis Martinez <luis_mmartinez@live.com>
2022-10-09 21:49:57 -0400

    solving time map; binary search; JavaScript

--- src/algorithms/binarySearch/timeMap.js
+++ src/algorithms/binarySearch/timeMap.js
@@ -1,6 +1,6 @@
 
 var TimeMap = function() {
-    
+    this.timeMap = {};
 };
 
 /** 
@@ -10,7 +10,11 @@ var TimeMap = function() {
  * @return {void}
  */
 TimeMap.prototype.set = function(key, value, timestamp) {
-    
+    if(!this.timeMap[key]) {
+        this.timeMap[key] = [{timestamp, value}];
+    } else {
+        this.timeMap[key].push({timestamp, value});
+    }
 };
 
 /** 
@@ -19,7 +23,7 @@ TimeMap.prototype.set = function(key, value, timestamp) {
  * @return {string}
  */
 TimeMap.prototype.get = function(key, timestamp) {
-    
+    return findTimeStamp(this.timeMap[key], timestamp) || ""
 };
 
 /** 
@@ -28,3 +32,20 @@ TimeMap.prototype.get = function(key, timestamp) {
  * obj.set(key,value,timestamp)
  * var param_2 = obj.get(key,timestamp)
  */
+
+function findTimeStamp(arr, timestamp) {
+    let left= 0;
+    let right = arr.length - 1;
+    if(arr.length === 0) return undefined
+    if(arr[right].timestamp <= timestamp) return arr[right].value;
+    let pivot;
+    while(left <= right) {
+        pivot = Math.floor((left + right)/2);
+        if(arr[pivot].timestamp <= timestamp && arr[pivot +1].timestamp > timestamp) {
+            return arr[pivot].value;
+        }
+        if(arr[pivot.timestamp < timestamp]) {
+            left = pivot + 1;
+        } else right = pivot -1;
+    }
+}
