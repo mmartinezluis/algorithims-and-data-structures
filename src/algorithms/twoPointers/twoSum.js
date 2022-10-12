4aeb1d32d17b7b59a7f193b458cda96f5ee4bafd
Luis Martinez <luis_mmartinez@live.com>
2022-10-11 21:41:41 -0400

    solved two sum II; two pointers; JavaScript

--- src/algorithms/twoPointers/twoSum.js
+++ src/algorithms/twoPointers/twoSum.js
@@ -4,5 +4,13 @@
  * @return {number[]}
  */
  var twoSum = function(numbers, target) {
-    
+    let i = 0;
+    let j = numbers.length-1;
+    let sum;
+    while(i < j) {
+        sum = numbers[i] + numbers[j];
+        if(sum === target) return [i+1,j+1];
+        if(sum < target){i +=1;}
+        else j -=1;
+    }
 };
