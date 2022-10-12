d9ca94d0a79c93077ff59146a97cc5a7c507dd24
Luis Martinez <luis_mmartinez@live.com>
2022-10-11 20:53:10 -0400

    solved valid palindrome; two pointers; JavaScript

--- src/algorithms/twoPointers/isPalindrome.js
+++ src/algorithms/twoPointers/isPalindrome.js
@@ -3,5 +3,14 @@
  * @return {boolean}
  */
  var isPalindrome = function(s) {
-    
+    let string = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    if(string.length <= 1) return true;
+    let i = 0;
+    let j = s.length - 1;
+    while(i <= j) {
+        if(string[i] !== string[j]) return false;
+        i+=1;
+        j-=1;
+    }
+    return true;
 };
