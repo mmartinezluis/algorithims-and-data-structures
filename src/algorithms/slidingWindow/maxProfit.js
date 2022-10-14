e1f8cc56b56ce96fa0eb64b7e2ebf779f55c1a6a
Luis Martinez <luis_mmartinez@live.com>
2022-10-13 22:50:02 -0400

    solved best time to buy and sell stock; sliding window; JavaScript

--- src/algorithms/slidingWindow/maxProfit.js
+++ src/algorithms/slidingWindow/maxProfit.js
@@ -3,6 +3,22 @@
  * @return {number}
  */
  var maxProfit = function(prices) {
-  
-    
+    let i = 0;
+    let min = Number.MAX_SAFE_INTEGER;
+    let max = 0;
+    let ans = 0;
+    while(i + 1 < prices.length) {
+        if(prices[i+1] > prices[i]) {
+            min = Math.min(min, prices[i]);
+            ans = Math.max(ans, prices[i+1] - min);
+        }
+        i++;
+    }
+    console.log(ans);
+    return ans;
 };
+
+let prices = [7,1,5,3,6,4]
+prices = [7,6,4,3,1]
+prices = [2,1,2,0,1]
+maxProfit(prices)
