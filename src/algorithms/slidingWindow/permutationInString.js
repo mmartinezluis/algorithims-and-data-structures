a0ac883004c3c161059ef46ece85ed60ef5a7f98
Luis Martinez <luis_mmartinez@live.com>
2022-10-20 00:25:55 -0400

    add minimum window substring; sliding window; JavaScript

--- src/algorithms/slidingWindow/permutationInString.js
+++ src/algorithms/slidingWindow/permutationInString.js
@@ -81,6 +81,7 @@ var checkInclusion = function(s1, s2) {
     return false;
 }
 
+// Solution 
 var checkInclusion = function(s1, s2) {
     if(s1.length > s2.length) return false;
     let count = {};
