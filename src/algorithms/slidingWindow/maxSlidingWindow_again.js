447c267332b76600594b9a8e8fcfc25f487b54ae
Luis Martinez <luis_mmartinez@live.com>
2022-10-24 17:28:17 -0400

    solving max sliding window

--- src/algorithms/slidingWindow/maxSlidingWindow_again.js
+++ src/algorithms/slidingWindow/maxSlidingWindow_again.js
@@ -4,7 +4,25 @@
  * @return {number[]}
  */
  var maxSlidingWindow = function(nums, k) {
-    
+    let heap = new MaxBinaryHeap();
+    let result = [];
+    for(let i=0; i < k; i++) {
+        heap.add({value: nums[i], index: i});
+    }
+    let max = heap.extractMax();
+    let i = 0;
+    result.push(max.value);
+    for(let j=k; j < nums.length; j++) {
+        i++;
+        heap.add({value: nums[j], index: j});
+        if(max.index < i || heap.values[0].value > max.value) {
+            max = heap.values[0];
+        }
+        result.push(max.value);
+        heap.extractMax();
+    }
+    console.log(result)
+    return result;
 };
 
 
@@ -22,7 +40,7 @@ class MaxBinaryHeap {
         let index = this.values.length -1;
         while(index > 0) {
             let node = this.values[index].value;
-            let parentIndex = Math.floor((this.values[index] -1)/2);
+            let parentIndex = Math.floor((index -1)/2);
             const parent = this.values[parentIndex].value;
             if(parent > node) break;
             swap(this.values, index, parentIndex);
@@ -33,18 +51,36 @@ class MaxBinaryHeap {
     extractMax() {
         let node = this.values[0];
         let end = this.values.pop();
-        if(this.values.length > 1) {
-            this.values.push(node);
+        if(this.values.length > 0) {
+            this.values[0] = end;
             this.bubbleDown();
         }
-        return end;
+        return node;
     }
 
     bubbleDown() {
-        
+        let index = 0;
+        while( index < this.values.length -1) {
+            const node = this.values[index].value;
+            let swapIndex;
+            let childIndex = 2*index + 1;
+            const child1 = this.values[childIndex];
+            const child2 = this.values[childIndex + 1];
+            if(child1 !== undefined && node < child1.value) swapIndex = childIndex;
+            if(child2 !== undefined && node < child2.value && child2.value > child1.value) swapIndex = childIndex +1;
+            if(swapIndex === undefined) break;
+            swap(this.values, swapIndex, index);
+            index = swapIndex;
+        }
     }
 }
 
 function swap(arr, index1, index2) {
     [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
 }
+
+
+let nums = [1,3,-1,-3,5,3,6,7] 
+let k = 1
+
+maxSlidingWindow(nums, k);
