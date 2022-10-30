644a6214def7fb38a7d681966e31a7f6aeb70f04
Luis Martinez <luis_mmartinez@live.com>
2022-10-29 22:40:31 -0400

    add word ladder; JavaScript

--- src/dataStructures/graphs/courseSchedule_again.js
+++ src/dataStructures/graphs/courseSchedule_again.js
@@ -39,10 +39,42 @@
 
 let numCourses = 2 
 let prerequisites = [[1,0],[0,1]]
-prerequisites = []
-canFinish(numCourses, prerequisites)
+// canFinish(numCourses, prerequisites)
+
 
 // Using DFS
+const WHITE = 1;
+const GRAY = 2;
+const BLACK = 3;
 var canFinish = function(numCourses, prerequisites) {
-    
+    const colors = new Array(numCourses).fill(WHITE);
+    const graph = new Map();
+    let isPossible = true;
+    let order = [];
+
+    for(let [u,v] of prerequisites) {
+        graph.has(v) ? graph.get(v).push(u) : graph.set(v, [u]);
+    }
+
+    for(let i=0; i < numCourses; i++) {
+        dfs(i);
+    }
+
+    function dfs(node) {
+        colors[node] = GRAY;
+        for(let neighbor of (graph.get(node) || [])) {
+            if(colors[neighbor] === WHITE) {
+                dfs(neighbor);
+            } else if(colors[neighbor] === GRAY) {
+                isPossible = false;
+            }
+        }
+        colors[node] = BLACK;
+        order.push(node);
+    }
+    console.log(isPossible)
+    return isPossible;
 };
+
+
+canFinish(numCourses, prerequisites)
