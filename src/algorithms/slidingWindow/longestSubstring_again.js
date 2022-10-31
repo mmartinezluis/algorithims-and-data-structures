85bb102eeaff85b0adc940e3a5176d2cdd9797a0
Luis Martinez <luis_mmartinez@live.com>
2022-10-30 21:53:12 -0400

    re-solved longest substring without repeating characters

--- src/algorithms/slidingWindow/longestSubstring_again.js
+++ src/algorithms/slidingWindow/longestSubstring_again.js
@@ -7,13 +7,17 @@
     let map = {};
     let max = 0;
     for(let right=0; right < s.length; right++) {
-        if(map[s[right] === undefined]) {
-            map[s[right]] = right;
-        } else {
-            left = Math.max(map[s[right]+1, left]);
-            map[s[right]] = right;
+        if(map[s[right]] !== undefined) {
+            left = Math.max( map[s[right]] + 1, left);
         }
-        max = Math.max(max, right - left);
+        max = Math.max(max, right - left + 1);
+        map[s[right]] = right;
     }
+    console.log(map, max)
     return max;
  }
+
+s="dgpqdtud"
+s= "pwwkew"
+s="abcdbah"
+lengthOfLongestSubstring(s);
