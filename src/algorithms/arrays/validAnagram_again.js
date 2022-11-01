9096d93039f928aac3b87534f92f9aa10a5b6eaf
Luis Martinez <luis_mmartinez@live.com>
2022-10-31 21:05:18 -0400

    re-solved valid anagram; arrays; JavaScript

--- src/algorithms/arrays/validAnagram_again.js
+++ src/algorithms/arrays/validAnagram_again.js
@@ -4,6 +4,16 @@
  * @return {boolean}
  */
  var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+    let sArray = new Array(26).fill(0);
+    let tArray = new Array(26).fill(0);
 
-
+    for(let i = 0; i < s.length; i++) {
+        sArray[s.charCodeAt(i) - 96]++;
+        tArray[t.charCodeAt(i) - 96]++;
+    }
+    for(let j=0; j < 26; j++) {
+        if(sArray[j] !== tArray[j]) return false;
+    }
+    return true;
  }
