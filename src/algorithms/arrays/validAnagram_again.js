689ac33a07581cb86b6fc04f1bf35e5d9355b9d3
Luis Martinez <luis_mmartinez@live.com>
2022-10-31 22:01:19 -0400

    re-solving group anagrams; arrays; JavaScript

--- src/algorithms/arrays/validAnagram_again.js
+++ src/algorithms/arrays/validAnagram_again.js
@@ -6,14 +6,13 @@
  var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
     let sArray = new Array(26).fill(0);
-    let tArray = new Array(26).fill(0);
-
+    
     for(let i = 0; i < s.length; i++) {
-        sArray[s.charCodeAt(i) - 96]++;
-        tArray[t.charCodeAt(i) - 96]++;
+        sArray[s.charCodeAt(i) - 97]++;
+        sArray[t.charCodeAt(i) - 97]--;
     }
     for(let j=0; j < 26; j++) {
-        if(sArray[j] !== tArray[j]) return false;
+        if(sArray[j] !== 0) return false;
     }
     return true;
  }
