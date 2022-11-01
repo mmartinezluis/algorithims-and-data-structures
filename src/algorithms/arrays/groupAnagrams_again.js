3fed09306390562fa469d234255f4759b9cb6340
Luis Martinez <luis_mmartinez@live.com>
2022-10-31 22:39:56 -0400

    re-solved group anagrams

--- src/algorithms/arrays/groupAnagrams_again.js
+++ src/algorithms/arrays/groupAnagrams_again.js
@@ -3,7 +3,18 @@
  * @return {string[][]}
  */
  var groupAnagrams = function(strs) {
-
-
-    
+    let map = new Map();
+    for(let str of strs) {
+        let codes = new Array(26).fill(0);
+        for(let char of str) {
+            codes[char.charCodeAt(0) - 97] += 1;
+        }
+        codes = codes.join();
+        map.has(codes) ? map.get(codes).push(str) : map.set(codes, [str]);
+    }
+    return [...map.values()];
  }
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+strs = ["bdddddddddd","bbbbbbbbbbc"]
+groupAnagrams(strs)
