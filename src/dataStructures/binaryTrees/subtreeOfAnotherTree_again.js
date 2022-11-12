0fb786dc9c3dfbe17fc1f9a673cd20024efbb7ad
Luis Martinez <luis_mmartinez@live.com>
2022-11-12 10:37:41 -0500

    re-solved subtree of another tree; using BSF and DFS

---
+++ src/dataStructures/binaryTrees/subtreeOfAnotherTree_again.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+ var isSubtree = function(root, subRoot) {
+    let queue = [root];
+    while(queue.length) {
+        const node = queue.shift();
+        if(node.val === subRoot.val && dfs(node, subRoot)) return true;
+        if(node.left) queue.push(node.left);
+        if(node.right) queue.push(node.right);
+    }
+    return false;
+ }
+
+ function sameTree(p, q) {
+    if(!p && !q) return true;
+    if(!p || !q || p.val !== q.val) return false;
+    return sameTree(p.left, q.left) && sameTree(p.right, q.right);
+ }
