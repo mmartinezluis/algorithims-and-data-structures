caacc68bdfef379ff52fc226d562175c7665a82b
Luis Martinez <luis_mmartinez@live.com>
2022-11-11 20:04:18 -0500

    re-solved binary tree level order traversal

--- src/dataStructures/binaryTrees/levelOrderTraversalBinaryTree_again.js
+++ src/dataStructures/binaryTrees/levelOrderTraversalBinaryTree_again.js
@@ -11,6 +11,19 @@
  * @return {number[][]}
  */
  var levelOrder = function(root) {
-
-
+    if(!root) return [];
+    let result = [];
+    let queue = [root];
+    while(queue.length) {
+        const levelLength = queue.length;
+        const level = [];
+        for(let i =0; i < levelLength; i++) {
+            const current = queue.shift();
+            level.push(current.val);
+            if(current.left) queue.push(current.left);
+            if(current.right) queue.push(current.right);
+        }
+        result.push(level);
+    }
+    return result;
  }
