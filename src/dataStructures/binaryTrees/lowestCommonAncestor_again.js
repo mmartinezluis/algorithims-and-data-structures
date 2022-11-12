9e03277a33b2b6fa04042c2634b7aa607eed843a
Luis Martinez <luis_mmartinez@live.com>
2022-11-11 21:21:18 -0500

    re-sovled lowest common ancestor of a binary tree; iterative solution

--- src/dataStructures/binaryTrees/lowestCommonAncestor_again.js
+++ src/dataStructures/binaryTrees/lowestCommonAncestor_again.js
@@ -12,11 +12,24 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+// Recursive solution
  var lowestCommonAncestor = function(root, p, q) {
-    return inOrderDFS(root, p.val, q.val);
-    function inOrderDFS(node, p, q) {
-        if(node.val > p && node.val > q && node.left) return inOrderDFS(node.left, p, q);
-        if(node.val < p && node.val < q && node.right) return inOrderDFS(node.right, p, q);
+    return dfs(root, p.val, q.val);
+    function dfs(node, p, q) {
+        if(node.val > p && node.val > q && node.left) return dfs(node.left, p, q);
+        if(node.val < p && node.val < q && node.right) return dfs(node.right, p, q);
         return node;
+    }
+ }
+
+// Iterative solution
+ var lowestCommonAncestor = function(root, p, q) {
+    let node = root;
+    p = p.val;
+    q = q.val;
+    while(node !== null) {
+        if(node.val > p && node.val > q){node = node.left;}
+        else if(node.val < p && node.val < q){node = node.right;}
+        else return node;
     }
  }
