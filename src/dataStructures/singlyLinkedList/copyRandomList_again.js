0db868d8d53cb42210320feaff34653d7f6ff845
Luis Martinez <luis_mmartinez@live.com>
2022-11-19 13:53:19 -0500

    re-sovled copy list with random pointer

--- src/dataStructures/singlyLinkedList/copyRandomList_again.js
+++ src/dataStructures/singlyLinkedList/copyRandomList_again.js
@@ -12,5 +12,19 @@
  * @return {Node}
  */
  var copyRandomList = function(head) {
+    let map = new Map();
+    let prehead = new Node();
+    let dummy = prehead;
+    let node = head;
+    map[null] = null;
 
+    while(node) {
+        if(!map.has(node)) map.set(node, new Node(node.val));
+        dummy.next = map.get(node);
+        if(node.random && !map.has(node.random)) map.set(node.random,new Node(node.random.val));
+        dummy.next.random = map.get(node.random);
+        dummy = dummy.next;
+        node = node.next;
+    }
+    return prehead.next;
  }
