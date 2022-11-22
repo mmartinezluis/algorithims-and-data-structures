ad0914d733ea6912469a861e01807952cebbd2ac
Luis Martinez <luis_mmartinez@live.com>
2022-11-21 19:46:48 -0500

    re-solved container with most water

--- src/algorithms/twoPointers/containerWithMostWater_again.js
+++ src/algorithms/twoPointers/containerWithMostWater_again.js
@@ -3,6 +3,18 @@
  * @return {number}
  */
  var maxArea = function(height) {
-
-
+    let l = 0;
+    let r = height.length -1;
+    let area = 0;
+    while(l < r) {
+        const width = r - l;
+        if(height[l] < height[r]) {
+            area = Math.max(area, height[l]*width);
+            l++;
+        } else {
+            area = Math.max(area, height[r]*width);
+            r--;
+        }
+    }
+    return area;
  }
