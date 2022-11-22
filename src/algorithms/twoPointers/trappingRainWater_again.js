321e63d69d0e981337a4db31be092fe75401272f
Luis Martinez <luis_mmartinez@live.com>
2022-11-21 19:20:51 -0500

    re-solved trapping rain water" "

--- src/algorithms/twoPointers/trappingRainWater_again.js
+++ src/algorithms/twoPointers/trappingRainWater_again.js
@@ -3,6 +3,23 @@
  * @return {number}
  */
  var trap = function(height) {
-
-    
+    let l =0;
+    let leftMax = 0;
+    let r = height.length -1;
+    let rightMax = 0;
+    let w = 0;
+    while(l < r) {
+        if(height[l] < height[r]) {
+            if(leftMax > height[l]) {
+                w += leftMax - height[l];
+            } else leftMax = height[l];
+            l++;
+        } else {
+            if(rightMax > height[r]) {
+                w += rightMax - height[r];
+            } else rightMax = height[r]
+            r--;
+        }
+    }
+    return w;
  }
