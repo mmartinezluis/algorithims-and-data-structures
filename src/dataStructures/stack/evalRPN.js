a248569998a1d6be0ad105863c101ac3e5814672
Luis Martinez <luis_mmartinez@live.com>
2022-11-24 09:52:36 -0500

    solving evaluate reverse polish notation

--- src/dataStructures/stack/evalRPN.js
+++ src/dataStructures/stack/evalRPN.js
@@ -2,6 +2,30 @@
  * @param {string[]} tokens
  * @return {number}
  */
- var evalRPN = function(tokens) {
-    
+ var evalRPN = function(tokens) {)
+    let stack = [];
+    let results;
+    for(let element of tokens) {
+        if(operations[element] === undefined) {
+            stack.push(element);
+        } else {
+            if(results === undefined) {
+                results = parseInt(stack.pop());
+            }
+            // results = stack.pop() + symbols[element] + results;
+            results = Math.ceil(operations[element](parseInt(stack.pop()), results));
+        }
+    }
+    console.log(results)
 };
+
+const operations = {
+    "+": (op1, op2) => op1 + op2,
+    "-": (op1, op2) => op1 - op2,
+    "*": (op1, op2) => op1*op2,
+    "/": (op1, op2) => op1/op2
+}
+
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+
+evalRPN(tokens)
