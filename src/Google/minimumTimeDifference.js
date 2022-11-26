be4fc7e3af5f5ab7d02ba104cc947dd75d4fcb65
Luis Martinez <luis_mmartinez@live.com>
2022-11-26 17:29:48 -0500

    solving minimum time difference

--- src/Google/minimumTimeDifference.js
+++ src/Google/minimumTimeDifference.js
@@ -2,6 +2,21 @@
  * @param {string[]} timePoints
  * @return {number}
  */
+
+ const MAX_TIME = 1440; 
+ 
  var findMinDifference = function(timePoints) {
-    
+   let records = new Set();
+   for(let point of timePoints) {
+     if(records.has(point)) return 0;
+     records.add(point);
+   }
+
 };
+
+function minutes(string1, string2) {
+    let time1 = ((parseInt(string1[0] + string1[1]))*60 + parseInt(string1[4] + string1[5])) || MAX_TIME;
+    let time2 = ((parseInt(string2[0] + string2[1]))*60 + parseInt(string2[4] + string2[5])) || MAX_TIME;
+    let minutes = time1 > time2 ? time1 - time2 : time2 - time1;
+    return minutes;
+}
