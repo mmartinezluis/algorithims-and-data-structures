635d5cb33ea6b5750fc9db47f933cc14514f44cb
Luis Martinez <luis_mmartinez@live.com>
2022-11-27 09:42:37 -0500

    solved minimum time difference

--- src/Google/minimumTimeDifference.js
+++ src/Google/minimumTimeDifference.js
@@ -3,20 +3,49 @@
  * @return {number}
  */
 
- const MAX_TIME = 1440; 
- 
- var findMinDifference = function(timePoints) {
-   let records = new Set();
-   for(let point of timePoints) {
-     if(records.has(point)) return 0;
-     records.add(point);
-   }
+const MAX_TIME = 1440; 
 
-};
+var findMinDifference = function(timePoints) {
+    let options = new Array(MAX_TIME).fill(false);
+    for(let timePoint of timePoints) {
+        const time = parseTime(timePoint);
+        if(options[time]) return 0;
+        options[time] =  true;
+    }
 
-function minutes(string1, string2) {
-    let time1 = ((parseInt(string1[0] + string1[1]))*60 + parseInt(string1[4] + string1[5])) || MAX_TIME;
-    let time2 = ((parseInt(string2[0] + string2[1]))*60 + parseInt(string2[4] + string2[5])) || MAX_TIME;
-    let minutes = time1 > time2 ? time1 - time2 : time2 - time1;
-    return minutes;
+    let low = 1500;
+    let high = -1;
+    let prev = 0;
+    let min = 1500;
+    for(let i=0; i < options.length; i++) {
+        if(options[i]) {
+            if(low !== 1500) {
+                min = Math.min(min, i - prev);
+            }
+            high = Math.max(high, i);
+            low = Math.min(low, i);
+            prev = i;
+        }
+    }
+    min = Math.min(min, MAX_TIME - high + low);
+    return min;
 }
+
+function parseTime(timePoint) {
+    const time = timePoint.split(":");
+    return parseInt(time[0])*60 + parseInt(time[1]);
+}
+
+function minutes(time1, time2) {
+    console.log(time1, time2)
+    let difference = Math.abs(time1 - time2);
+    if(difference > 720) {
+        if(time1 > 720) {
+            difference = MAX_TIME - time1 + time2
+        } else difference = MAX_TIME - time2 + time1
+    }
+    return difference;
+}
+
+let timePoints = ["23:59","00:00"]
+findMinDifference(timePoints)
