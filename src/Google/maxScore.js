68fc7cd4460d66f209bf67c8164023bbbe7a2baf
Luis Martinez <luis_mmartinez@live.com>
2022-11-27 19:24:32 -0500

    solving max score

--- src/Google/maxScore.js
+++ src/Google/maxScore.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
  var maxScore = function(cardPoints, k) {
-    
+    let count = 0;
+    let total = 0;
+    let left = 0;
+    let right = cardPoints.length -1;
+    while(left < right || count < k) {
+        if(cardPoints[left] > cardPoints[right]) {
+            total += cardPoints[left];
+            left++;
+        } else {
+            total += cardPoints[right];
+            right++;
+        }
+        count++;
+    }
+    return total;
 };
