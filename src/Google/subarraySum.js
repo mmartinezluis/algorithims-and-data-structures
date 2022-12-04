920664100918c36434ece48822b8f1600d71165a
Luis Martinez <luis_mmartinez@live.com>
2022-12-04 18:10:55 -0500

    add  cumulative sum solution with constant space for subarray sum equals k

--- src/Google/subarraySum.js
+++ src/Google/subarraySum.js
@@ -27,10 +27,26 @@ var subarraySum = function(nums, k) {
     for(let i = 1; i <= nums.length; i++) {
         cumSum[i] = cumSum[i - 1] + nums[i - 1];
     }
-    let count=0;
-    for(let start= 0; start < nums.length; start++) {
+    let count = 0;
+    for(let start = 0; start < nums.length; start++) {
         for(let end= start + 1; end <= nums.length; end++) {
             if((cumSum[end] - cumSum[start]) === k) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+// Using cumulative sum, contant space; solves the problem but runs out of time
+var subarraySum = function(nums, k) {
+    let count = 0;
+    for(let start=0; start < nums.length; start++) {
+        let sum = 0;
+        for(let end=start; end < nums.length; end++) {
+            sum += nums[end];
+            if(sum === k) {
                 count++;
             }
         }
