6df6a476d1c1b93924e3c39003b6d957341d1ed7
Luis Martinez <luis_mmartinez@live.com>
2022-12-04 18:30:37 -0500

    add hasmap solution for subarray sum equals k

--- src/Google/subarraySum.js
+++ src/Google/subarraySum.js
@@ -53,3 +53,17 @@ var subarraySum = function(nums, k) {
     }
     return count;
 }
+
+// Using a hashmap; O(n) solution
+var subarraySum = function(nums, k) {
+    let count = 0;
+    let sum = 0;
+    let map = new Map();
+    map.set(0,1);
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if(map.has(sum - k)) count += map.get(sum - k);
+        map.set(sum, (map.get(sum)+1) || 1);
+    }
+    return count;
+}
