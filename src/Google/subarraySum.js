3ee50841452c747edaa3d24229536d05f2ea9077
Luis Martinez <luis_mmartinez@live.com>
2022-12-04 17:34:21 -0500

    add cumulative sum solution for subarray sum equals k

--- src/Google/subarraySum.js
+++ src/Google/subarraySum.js
@@ -20,7 +20,20 @@
     return count;
 };
 
-// Using cumulative sum
+// Using cumulative sum; solves the problem but runs out of time
 var subarraySum = function(nums, k) {
-
+    let cumSum = new Array(nums.length + 1);
+    cumSum[0] = 0;
+    for(let i = 1; i <= nums.length; i++) {
+        cumSum[i] = cumSum[i - 1] + nums[i - 1];
+    }
+    let count=0;
+    for(let start= 0; start < nums.length; start++) {
+        for(let end= start + 1; end <= nums.length; end++) {
+            if((cumSum[end] - cumSum[start]) === k) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
