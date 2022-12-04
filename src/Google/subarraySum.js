e6b10db8ba6cb6be741799fa247dd8c92362bfb2
Luis Martinez <luis_mmartinez@live.com>
2022-12-04 16:16:09 -0500

    solving subarray sum equals k

--- src/Google/subarraySum.js
+++ src/Google/subarraySum.js
@@ -3,6 +3,24 @@
  * @param {number} k
  * @return {number}
  */
+
+// Brute force solution:
+// Use nested loops to iterate over all the possible subarrays
  var subarraySum = function(nums, k) {
-    
+    let count = 0;
+    for(let start = 0; start < nums.length; start++) {
+        for(let end= start+1; end <= nums.length; end++) {
+            let sum = 0;
+            for(let i= start; i < end; i++) {
+                sum += nums[i];
+                if(sum === k) count++;
+            }
+        }
+    }
+    return count;
 };
+
+// Using cumulative sum
+var subarraySum = function(nums, k) {
+
+}
