d71174237d802609ed00fb1e0369912f7059d564
Luis Martinez <luis_mmartinez@live.com>
2022-12-04 18:48:57 -0500

    solved two sum

---
+++ src/Google/twoSum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(let i=0; i < nums.length; i++) {
+        const delta = target - nums[i];
+        if(map.has(delta) && map.get(delta) !== i) {
+            return [map.get(delta), i]
+        }
+        map.set(nums[i], i);
+    }
+}
+
+// x + y = t
+// x - t = -y
