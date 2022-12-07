9ae83e19aeaadaa854aa902bbf4625c1a6d1e5eb
Luis Martinez <luis_mmartinez@live.com>
2022-12-07 11:27:56 -0500

    solved happy number

--- src/Google/isHappy.js
+++ src/Google/isHappy.js
@@ -3,5 +3,18 @@
  * @return {boolean}
  */
  var isHappy = function(n) {
-    
+    const set = new Set();
+    let number = n;
+    let sum;
+    while(number !== 1) {
+        number = "" + number;
+        sum = 0;
+        for(let i = 0; i < number.length; i++){
+            sum += parseInt(number[i])**2;
+        };
+        if(set.has(sum)) return false;
+        set.add(sum);
+        number = sum;
+    }
+    return true;
 };
