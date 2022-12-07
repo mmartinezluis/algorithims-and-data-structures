16980f736e2ad783c148bd2e9ba9578ee841e07b
Luis Martinez <luis_mmartinez@live.com>
2022-12-07 16:54:25 -0500

    solving first bad version

--- src/Google/firstBadVersion.js
+++ src/Google/firstBadVersion.js
@@ -18,6 +18,23 @@
      * @return {integer} The first bad version
      */
     return function(n) {
-        
+        let i = 1;
+        let j = n;
+        let prevApi =null
+        let prevNumber =1;
+        let current = 1;
+        while(i < j) {
+            current = Math.floor((j+i)/2);
+            const api = isBadVersion(current);
+            if(api && prevApi !== null && !prevApi) return prevNumber + 1;
+            if(api) {
+                j = current -1;
+            } else {
+                prevNumber = current;
+                i = current + 1;
+            }
+            prevApi = api;
+        }
+        return prevNumber;
     };
 };
