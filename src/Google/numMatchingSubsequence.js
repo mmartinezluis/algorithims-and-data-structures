6baeabdbd387273640e6ed7ba077e90b746619f1
Luis Martinez <luis_mmartinez@live.com>
2023-01-04 00:15:37 -0500

    solving number of matching subsequences

--- src/Google/numMatchingSubsequence.js
+++ src/Google/numMatchingSubsequence.js
@@ -3,4 +3,23 @@
  * @param {string[]} words
  * @return {number}
  */
-var numMatchingSubseq = function (s, words) {};
+var numMatchingSubseq = function (s, words) {
+  const bluePrint = new Array(26).fill([]);
+  for (let i = 0; i < s.length; i++) {
+    bluePrint[s.charCodeAt(i) - 97].push(i);
+  }
+
+  let lastIndex = -1;
+  let count = 0;
+  for (let word of words) {
+    const copy = [...bluePrint];
+    for (let i = 0; i < word.length; i++) {
+      const indeces = copy[word.charCodeAt(i) - 97];
+      if (!indeces.length || indeces[word.length - 1] < lastIndex) break;
+      while (indeces[0] < lastIndex) indeces.shift();
+      lastIndex = indeces.shift();
+      if (i === word.length - 1) count++;
+    }
+  }
+  return count;
+};
