6b74be642da003621e5418eff59788f5971173bc
Luis Martinez <luis_mmartinez@live.com>
2023-01-04 23:58:12 -0500

    solving number of matching subsequences

--- src/Google/numMatchingSubsequence.js
+++ src/Google/numMatchingSubsequence.js
@@ -4,22 +4,36 @@
  * @return {number}
  */
 var numMatchingSubseq = function (s, words) {
-  const bluePrint = new Array(26).fill([]);
+  const bluePrint = [...Array(26)].map(() => []);
   for (let i = 0; i < s.length; i++) {
     bluePrint[s.charCodeAt(i) - 97].push(i);
   }
 
   let lastIndex = -1;
   let count = 0;
+  let pointer;
   for (let word of words) {
-    const copy = [...bluePrint];
+    const lastPosition = new Map();
     for (let i = 0; i < word.length; i++) {
-      const indeces = copy[word.charCodeAt(i) - 97];
-      if (!indeces.length || indeces[word.length - 1] < lastIndex) break;
-      while (indeces[0] < lastIndex) indeces.shift();
-      lastIndex = indeces.shift();
+      const letter = word[i];
+      const index = word.charCodeAt(i) - 97;
+      if (
+        !bluePrint[index].length ||
+        bluePrint[index][word.length - 1] <= lastIndex
+      )
+        break;
+      pointer = lastPosition.has(letter) ? lastPosition.get(letter) : 0;
+      while (bluePrint[index][pointer] < lastIndex) pointer++;
+      lastIndex = bluePrint[index][pointer];
       if (i === word.length - 1) count++;
+      lastPosition.set(letter, pointer);
     }
+    lastIndex = -1;
   }
+  console.log(count);
   return count;
 };
+
+let s = "abcde";
+let words = ["a", "bb", "acd"];
+numMatchingSubseq(s, words);
