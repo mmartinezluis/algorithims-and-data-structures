5b807332eda989e40aa739ffa080be6e2f6d52bb
Luis Martinez <luis_mmartinez@live.com>
2023-01-10 18:41:14 -0500

    solved flipping an image

--- src/algorithms/arrays/flippingImage.js
+++ src/algorithms/arrays/flippingImage.js
@@ -2,4 +2,10 @@
  * @param {number[][]} image
  * @return {number[][]}
  */
-var flipAndInvertImage = function (image) {};
+var flipAndInvertImage = function (image) {
+  for (let i = 0; i < image.length; i++) {
+    image[i].reverse();
+    image[i] = image[i].map((el) => (el === 0 ? 1 : 0));
+  }
+  return image;
+};
