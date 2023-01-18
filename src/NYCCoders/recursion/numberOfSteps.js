5187d9b6b5d71b36522af9021893370124f0ed9e
Luis Martinez <luis_mmartinez@live.com>
2023-01-17 19:24:34 -0500

    add number of steps

---
+++ src/NYCCoders/recursion/numberOfSteps.js
@@ -0,0 +1,5 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {};
--- src/NYCCoders/recursion/reverseString.js
+++ src/NYCCoders/recursion/reverseString.js
@@ -12,3 +12,33 @@ var reverseString = function (s) {
     r--;
   }
 };
+
+// Recursive solution; new array
+var reverseString = function (s, arr = []) {
+  if (s.length === 1) {
+    console.log(arr);
+    return arr;
+  }
+  arr.push(s.pop());
+  return reverseString(s, arr);
+};
+
+// Recursive solution
+var reverseString = function (s) {
+  if (s.length === 1) return s[0];
+  return [...reverseString(s.slice(1)), s[0]];
+};
+
+let s = ["h", "e", "l", "l", "o"];
+reverseString(s);
+
+// Recursive solution
+var reverseString = function (s) {
+  helper(s, 0, s.length - 1);
+};
+
+function helper(arr, l, r) {
+  if (l >= r) return;
+  [arr[l], arr[r]] = [arr[l], arr[r]];
+  helper(arr, l + 1, r - 1);
+}
