732864897711bb060a4065c4391b7e13b34b6e76
Luis Martinez <luis_mmartinez@live.com>
2023-01-17 19:31:30 -0500

    solved number of steps

--- src/NYCCoders/recursion/numberOfSteps.js
+++ src/NYCCoders/recursion/numberOfSteps.js
@@ -2,4 +2,14 @@
  * @param {number} num
  * @return {number}
  */
-var numberOfSteps = function (num) {};
+var numberOfSteps = function (num) {
+  let count = 0;
+  return recursive(num);
+
+  function recursive(num) {
+    if (num === 0) return count;
+    count++;
+    if (num % 2 === 0) return recursive(num / 2);
+    return recursive(num - 1);
+  }
+};
